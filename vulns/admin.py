beefc66793ba45ef1a659050ad8107bbc27d247e
juebonatzzo <juebonatzzo@gmail.com>
2020-04-23 17:12:09 +0200

    adding basic models and admin

--- vulns/admin.py
+++ vulns/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Vulnerability,CVE
 # Register your models here.
+
+admin.site.register(CVE)
+
+
+class CVEInline(admin.TabularInline):
+    model = CVE
+
+
+@admin.register(Vulnerability)
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ['name','state','cves_display']
+    list_filter = ['state']
+    inlines = [CVEInline]
+
+    def cves_display(self, obj):
+        return ", ".join([
+            child.cve_id for child in obj.cves.all()
+        ])
+    cves_display.short_description = "CVEs"
--- vulns/models.py
+++ vulns/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+VULNERABILITY_STATES = [
+    (0, 'Created'),
+    (10, 'Study'),
+    (20, 'Reported'),
+    (30, 'Monitoring'),
+    (40, 'Closed'),
+]
+
+
+class Vulnerability(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    state = models.IntegerField(choices=VULNERABILITY_STATES, default=0,)
+
+
+class CVE(models.Model):
+    cve_id = models.CharField(max_length=30, unique=True)
+    vulnerability = models.ForeignKey(Vulnerability, on_delete=models.CASCADE, related_name='cves')
+
+
