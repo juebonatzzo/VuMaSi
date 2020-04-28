33713d5f7f32f9dca3a1b238d1ee1349425313cf
juebonatzzo <juebonatzzo@gmail.com>
2020-04-28 22:27:04 +0200

    adding features, filters, email, cvs export

--- vulns/admin.py
+++ vulns/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from .models import Vulnerability,CVE
-# Register your models here.
+import csv
+from django.http import HttpResponse
 
 admin.site.register(CVE)
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
 
 
 class CVEInline(admin.TabularInline):
@@ -10,13 +30,23 @@ class CVEInline(admin.TabularInline):
 
 
 @admin.register(Vulnerability)
-class HeroAdmin(admin.ModelAdmin):
-    list_display = ['name','state','cves_display']
-    list_filter = ['state']
+class HeroAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = ['name','state','cves_display','modified_date']
+    list_filter = ['state','creation_date','impact']
+    search_fields = ['name','description','solution','cves__cve_id']
     inlines = [CVEInline]
+    actions = ["export_as_csv","send_mail"]
 
     def cves_display(self, obj):
         return ", ".join([
             child.cve_id for child in obj.cves.all()
         ])
     cves_display.short_description = "CVEs"
+
+    def send_mail(self, request, queryset):
+        pass
+
+    send_mail.short_description = "Send Email Selected"
+
+    # list_editable = ['state']
+    # date_hierarchy = 'creation_date'
--- vulns/models.py
+++ vulns/models.py
@@ -8,14 +8,43 @@ VULNERABILITY_STATES = [
     (40, 'Closed'),
 ]
 
+VULNERABILITY_IMPACT = [
+    (0, 'Informational'),
+    (10, 'Low'),
+    (20, 'Medium'),
+    (30, 'High'),
+    (40, 'Critical'),
+]
+
 
 class Vulnerability(models.Model):
     name = models.CharField(max_length=200, unique=True)
     state = models.IntegerField(choices=VULNERABILITY_STATES, default=0,)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    description = models.TextField(null=True)
+    impact = models.IntegerField(choices=VULNERABILITY_IMPACT, default=0)
+    exploited_in_the_wild = models.BooleanField(default=False)
+    solution = models.TextField(null=True)
+    notes = models.TextField(null=True)
+    # links
+    # attack_vector
+    # vulnerable_products
+    # affectation
+
+    class Meta:
+        verbose_name_plural = "Vulnerabilities"
+
+    def __str__(self):
+        return self.name
 
 
 class CVE(models.Model):
     cve_id = models.CharField(max_length=30, unique=True)
+    cve_cvss_score = models.FloatField(null=True, blank=True)
+    cve_cvss_vector = models.CharField(max_length=100, null=True, blank=True)
+
     vulnerability = models.ForeignKey(Vulnerability, on_delete=models.CASCADE, related_name='cves')
 
-
+    def __str__(self):
+        return self.cve_id
