29f0a800b99570ead20256b46d910d32e0253d7b
juebonatzzo <juebonatzzo@gmail.com>
2020-04-28 23:04:35 +0200

    adding custom titles to admin portal

--- vumasi/urls.py
+++ vumasi/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "RSI Admin"
+admin.site.site_title = "RSI Admin Portal"
+admin.site.index_title = "Welcome to RSI Researcher Portal"
